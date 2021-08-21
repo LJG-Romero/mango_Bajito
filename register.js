db80d2ab944f302f375b639a93daa2f2bd8ad373
LJG-Romero <luucas.jg@gmail.com>
2021-08-20 23:38:05 -0300

    Avances sección Register. Validación de disponibilidad del nombre de usuario y corrección de detalles mensajes validaciones contraseña

--- register.js
+++ register.js
@@ -1,11 +1,31 @@
+                        /* Remaining taks */
+
+    // Falta desarrollo de chequeo de disponibilidad de nombre de usuario en tiempo real. ******** CHECK
+    // Falta input de email.
+    // Falta agregarle ID al constructor Register. ******** CHECK
+    // Falta acomodar ID una vez que regrese a Register 
+    // Falta desarrollar chequeo de campos obligatorios.
+    // Falta desarrollar que la edad sea +18.
+    // Falta desarrollo de personalización input de pass cuando no se cumple la condicion con CSS.
+    // Falta desarrollo para chequear formato de tipos de caracteres para contraseña e email.
+    // Falta desarrollo checkbox y algoritmo para personalizar mensaje de bienvenida por género.
+    // Falta implementar asinc o similar para transción entre windows.
+    // Falta implementar import y export para reutilizar atributos de los usuarios para las tarjetas de cardManager.
+
+    // Falta desarrollar css.
+    // Falta implementar efectos btns.
+    // Falta implementar los vector ilustrations.
+
 class Register{
-    constructor(nombre, apellido, fechaDeNacimiento, usuario, contraseña){
+    constructor(nombre, apellido, fechaDeNacimiento, usuario, contraseña, id){
         this.nombre = nombre;
         this.apellido = apellido;
         this.fechaDeNacimiento = fechaDeNacimiento;
         this.usuario = usuario;
         this.contraseña = contraseña;
+        this.id = counter++;
     }
+        /* Class method example */
     // saludar(nombre){
     //     alert("Bienvenido " + this.nombre + " !");
 
@@ -13,12 +33,66 @@ class Register{
 } 
 
 let passCheck = false;
+let counter = 1;
+
+/* Live validation of user name availability  */
+
+let userValidation = document.getElementById("user");
+userValidation.addEventListener("input", nameValidation);
+
+function nameValidation(){
+
+        /* Bloque a refactorizar con una función */
+    /* Peticion de users DB */
+    let usersContainerLocal = localStorage.getItem("arrayUsers");
+    let usersContainer = JSON.parse(usersContainerLocal);
+    
+    let node = document.getElementById("errorMsg1");
+    let element = document.createElement("p");
+
+    if(usersContainerLocal = usersContainer != null){
+        
+        for (const users of usersContainer) {
+            // console.log(userValidation.value); CHEQUEO funcionamiento ciclo
+            // console.log(users.usuario);
+
+            if(userValidation.value == users.usuario){
+                //console.log("coinciden")
+                node.innerHTML = "";
+                element.innerHTML = `Ups ! ${userValidation.value} no está 
+                                    disponible.`
+                                    
+                                    // Intenta otra vez, mango${userValidation.value} no suena 
+                                    // tan mal no? ... Mi programador se cree con mucho 'humor' ${String.
+                                    // fromCodePoint(0x1F612)}`
+                node.appendChild(element);
+                break;
+            }
+            else if (userValidation.value != users.usuario){
+                //console.log("o entra por aca")
+                node.innerHTML = "";
+                element.innerHTML = `${userValidation.value} se oye bien. Sigamos !`
+                node.appendChild(element);
+            }
+        }
+    }
+    else{
+        node.innerHTML = "";        
+        element.innerHTML = `${userValidation.value} se oye bien. Sigamos !`
+        node.appendChild(element);
+        if(userValidation.value == ""){
+            node.innerHTML = "";
+        }
+    }
+}
+
+
+/* User factory */
 
 let userGenerator = document.getElementById("btnUser");
 userGenerator.addEventListener("click", usersFactory);
 
-function usersFactory(){
-    
+function usersFactory(e){
 
     let name = document.getElementById("name").value,
         lastname = document.getElementById("lastname").value,
@@ -27,27 +101,18 @@ function usersFactory(){
         password = document.getElementById("password").value,
         passwordCheck = document.getElementById("passwordCheck").value;
 
-
-    // Falta desarrollo de chequeo de disponibilidad de nombre de usuario en tiempo real.
-
-        //let node = getElementById("errorMsg1");
-        //let element = document.createElement("p");
-
-    // Falta input de email.
-    // Falta desarrollo de personalización input de pass cuando no se cumple la condicion.
-    // Falta desarrollo para chequear formato de contraseña e email.
-    // Falta desarrollo checkbox y algoritmo para personalizar mensaje de bienvenida por género.
-    // Falta implementar asinc o similar para trasnción entre windows.
-    // Falta implementar import y export para reutilizar atributos de los usuarios para las tarjetas de cardManager.
-
-    // Falta desarrollar css.
-    // Falta implementar los vector ilustrations.
-
-
-
-    if(password === passwordCheck){
-        //console.log("Funciona checkeo contras");
-
+    if(password === passwordCheck && password != "" && passwordCheck != "" && user != ""){
+        // (password = passwordCheck != "") && user != "" Esta versión mas corta no funcionó. Pass almacena true ! 
+        //console.log("Funciona checkeo campos");
+        
+            /* Bloque a refactorizar con una función */
+        /* Reseteo users Msgs */
+        let temp2 = document.getElementsByClassName("errorMsg2")
+        for (const nodes of temp2){
+            nodes.innerHTML = "";
+        }
+            /* Bloque a refactorizar con una función */
+        /* Peticion de users DB */
         let usersContainerLocal = localStorage.getItem("arrayUsers");
         let usersContainer = JSON.parse(usersContainerLocal);
 
@@ -55,37 +120,40 @@ function usersFactory(){
         if(usersContainerLocal == null && usersContainer == null){
             usersContainerLocal = "";
             usersContainer = [];
-
             // console.log(usersContainerLocal); // Chequeo el storage local y de origen.
             // console.log(usersContainer);
-
         }
         
-    
         usersContainer.push(new Register(name, lastname, bird, user, password));
+        
         usersContainerLocal = JSON.stringify(usersContainer);
         localStorage.setItem("arrayUsers", usersContainerLocal);
-        let node1 = document.getElementById("registerContainer");
-        node1.innerHTML= `<h2 style="color:tomato; font-size: 50px; text-align:center;">
+
+            /* Msg welcom user */
+        // let node1 = document.getElementById("registerContainer");
+        // node1.innerHTML= `<h2 style="color:tomato; font-size: 50px; text-align:center;">
         
-        ${name} ${lastname}, bienvenido a la comunidad Mango Bajito !
+        //                    ${name} ${lastname}, bienvenido a la comunidad Mango Bajito !
         
         
-        </h2>`
+        //                     </h2>`
+
         alert("Creación de usuario exitoso");
         alert("Inicia sesión nuevamente");
+
+        e.preventDefault(); // Provisorio para poder realizar el redireccionamiento a sección SingIn
+
         window.location.href = "signIn.html";
-
-        // document.getElementById("registerContainer").innerHTML = `<h2 style="color:tomato; font-size: 50px; text-align:center;">
-                            
-        //                     ${name} ${lastname}, bienvenido a la comunidad Mango Bajito !
-        
-        
-        //                   </h2>`
-
     }
+    // else if (password == "" || passwordCheck == "" || user == ""){
+
+    // }
     else{
+        e.preventDefault();
+
         if(passCheck){
+                /* Bloque a refactorizar con una función */
+            /* Reseteo users Msgs */
             let temp = document.getElementsByClassName("errorMsg2")
             for (const nodes of temp){
                 nodes.innerHTML = "";
@@ -104,7 +172,7 @@ function usersFactory(){
             //console.log(nodes); CHEQUEO elem. "array". 
         }
     }
-}  
+}
 
 
 
